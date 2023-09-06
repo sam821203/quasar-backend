9e17b3bc4a4e6c060a9f0f4099a6a9ebb15d1717
yuhao <sam821203@yahoo.com.tw>
2023-09-06 14:19:02 +0800

    feat: set access control allow origin to global

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
+// dependencies
 const express = require('express')
+const serviceAccount = require('./serviceAccountKey.json');
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 // config - express
 const app = express()
+// config - firebase
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
 
 // endpoint - posts
-app.get('/posts', (req, res) => {
-  const posts = [
-    {
-      caption: 'Golden Japan man',
-      location: 'Japan',
-    },
-    {
-      caption: 'Gate Bridge',
-      location: 'San Francisco, United States',
-    }
-  ]
+app.get('/posts', async (req, res) => {
+  const posts = []
+  const snapshot = await db.collection('posts').orderBy('date', 'desc').get();
+
+  // 允許任何服務都可以取得該 service point
+  res.set('Access-Control-Allow-Origin', '*')
+
+  snapshot.forEach((doc) => {
+    posts.push(doc.data());
+  });
+
   res.send(posts)
+})
+
+// endpoint - create posts
+app.get('/createPosts', async (req, res) => {
+
+  // 允許任何服務都可以取得該 service point
+  res.set('Access-Control-Allow-Origin', '*')
+  res.send('createPosts')
 })
 
 app.listen(3000, () => {
